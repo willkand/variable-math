4c973bea3045064e5681cb73a4d6b06344d16a40
willkand <will.kirkland@codeimmersives.com>
2022-05-20 15:34:41 -0400

    1st commit distance.js

---
+++ distance.js
@@ -0,0 +1,11 @@
+let x1 = 2;
+let x2 = 2;
+let y1 = 1;
+let y2 = 5;
+console.log("√(x2 - x1)^2 + (y2 - y1)^2");
+console.log("x2 = 2, x1 = 2, y2 = 5, y1 = 1");
+console.log("√(2-2)^2 + (5-1)^2");
+console.log("√(0)^2 + (4)^2");
+console.log("√0 + 16");
+console.log("√16");
+console.log(+Math.sqrt(x2 - x1)^2 + (y2 - y1)^2);
