c9bc39d8db26f9dcea9052642ba132293f52bd2f
willkand <will.kirkland@codeimmersives.com>
2022-05-20 17:28:41 -0400

    1st commit quadritc.js

---
+++ quadratic.js
@@ -0,0 +1,9 @@
+let a = 2
+let b = 3
+let c = -2
+
+console.log(((-b) +Math.sqrt(b ** 2 -4 * (a) * (c)))/(2 * a))
+
+console.log(((-b) -Math.sqrt(b ** 2 -4 * (a) * (c)))/(2 * a))
+
+console.log("x1= 2/4 or .5 and x2= -8/4 or -2")
