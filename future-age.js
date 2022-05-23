0366b74794ff57f97a1aa5576f755fdafa71a742
willkand <will.kirkland@codeimmersives.com>
2022-05-23 10:42:29 -0400

    future age 2nd commot

--- future-age.js
+++ future-age.js
@@ -1,3 +1,3 @@
-let age = 37
-let age2 = age + 5
-console.log("I am " + age + " but in 5 years I will be " + age2 + " years old. ");
+let age = 37;
+let age2 = age + 5;
+console.log("I am " + age + ", but in 5 years I will be " + age2 + " years old. ");
